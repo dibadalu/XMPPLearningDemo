59d84aaa2a499afca326a82096a6edb6c08843ec
dibadalu <1004205150@qq.com>
2015-12-05 13:00:20 +0800

    新建项目

---
+++ WeChatDemo/WeChatDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChatDemo
+//
+//  Created by 陈泽嘉 on 15/12/5.
+//  Copyright (c) 2015年 dibadalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChatDemo/WeChatDemoTests/WeChatDemoTests.m
@@ -0,0 +1,40 @@
+//
+//  WeChatDemoTests.m
+//  WeChatDemoTests
+//
+//  Created by 陈泽嘉 on 15/12/5.
+//  Copyright (c) 2015年 dibadalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface WeChatDemoTests : XCTestCase
+
+@end
+
+@implementation WeChatDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
